e8cdf700a19b5842204eedcc9105cbabc6821422
felpsalvs <profelipealves@outlook.com>
2024-10-11 23:41:03 -0300

    feat(components): update support toggling

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,39 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import { IoMoon, IoSunny } from 'react-icons/io5'
 
 const themes = ['light', 'dark']
 
-export default function ThemeToggle(){
-    const [isMounted, setIsMounted] = useState(false)
-    const [theme, setTheme] = useState(() => {
-        if (import.meta.env.SSR){
-            return undefined
-        }
-        if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')){
-            return localStorage.getItem('theme')
-        }
-        if (window.matchMedia('(prefers-color-schheme: dark)').matches){
-            return 'dark'
-        }
-        return 'light'
-    })
-    const toggleTheme = () => {
-        const t = theme === 'light' ? 'dark' : 'light'
-        localStorage.setItem('theme', t)
-        setTheme(t)
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
     }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-schheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
 
-    useEffect(() => {
-        const root = document.documentElement
-        if (theme === 'light'){
-            root.classList.remove('dark')
-        } else {
-            root.classList.add('dark')
-        }
-    }, [theme])
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
-    return isMounted ? <div className="inline-flex">Button</div>
+  return isMounted ? (
+    <div className="inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-zinc-600">
+      {themes.map(t => {
+        const checked = t === theme
+        return (
+          <button
+            key={t}
+            onClick={toggleTheme}
+            aria-label="Toggle theme"
+            className={`${
+              checked ? 'bg-white' : ''
+            } cursor-pointer rounded-3xl p-2`}
+          >
+            {t === 'light' ? <IoSunny /> : <IoMoon />}
+          </button>
+        )
+      })}
+    </div>
+  ) : (
+    <div />
+  )
 }

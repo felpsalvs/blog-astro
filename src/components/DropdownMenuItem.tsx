853589e14617ad4e4a81ce077de7d60cdcffe054
felpsalvs <profelipealves@outlook.com>
2024-10-30 16:29:25 -0300

    chore: added dropdown items

---
+++ src/components/DropdownMenuItem.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from "react"
+import { MenuItem } from '@headlessui/react'
+import React from 'react'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type Props = {
+  href: string
+  children: ReactNode
+}
+
+export default function DropdownMenuItem({ href, children }: Props) {
+  return (
+    <MenuItem>
+      {({ focus }) => (
+        <a href={href}
+          className={classNames(
+            focus ? 'bg-orange-200 dark:bg-zinc-700' : '',
+            'block px-4 py-2 text-sm'
+          )}>{children}</a>
+      )}
+    </MenuItem>
+  )
+}
